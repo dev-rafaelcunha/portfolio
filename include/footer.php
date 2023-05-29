2e7421b00f302055cabbc6dbd1686e4728b7a92f
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2023-05-29 14:47:36 -0300

    Ajuste no padding footer

--- include/footer.php
+++ include/footer.php
@@ -1,13 +1,12 @@
-<footer class="py-4 fixed-bottom text-light d-flex justify-content-between px-5" style="background-color: #000;">
+<footer class="py-3 fixed-bottom text-light d-flex justify-content-between px-5" style="background-color: #000;">
     <div class="col d-flex justify-content-start d-lg-flex d-none"></div>
     <div class="col text-center">
         <p class="fs-7">&copy; Portfólio desenvolvido em:</p>
-        <span class="mx-3 text-secondary">PHP</span>
-        <span class="mx-3 text-secondary">MySQL</span>
-        <span class="mx-3 text-secondary">JavaScript</span>
         <span class="mx-3 text-secondary">HTML</span>
         <span class="mx-3 text-secondary">CSS</span>
         <span class="mx-3 text-secondary">Bootstrap</span>
+        <span class="mx-3 text-secondary">JavaScript</span>
+        <span class="mx-3 text-secondary">PHP</span>
     </div>
     <div class="col d-flex justify-content-end align-items-center d-lg-flex d-none">
         <p class="text-danger m-0">&copy; Rafael Cunha</p>
