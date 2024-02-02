ef60a062bf4c643af0c1c509ecdf310dfe95c589
dev-rafaelcunha <dev.rafaelcunha@gmail.com>
2024-02-02 11:34:02 -0300

    Ajustes responsividade Footer.

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
-<footer class="py-3 fixed-bottom text-light d-flex justify-content-between px-5 py-4" style="background-color: #000;">
-    <div class="col d-flex justify-content-start d-lg-flex d-none align-items-center">
+<footer class="fixed-bottom text-light d-flex justify-content-between p-4" style="background-color: #000;">
+    <div class="col d-flex justify-content-start d-xl-flex d-none align-items-center">
         <span class="mx-3">HTML</span>
         <span class="mx-3">CSS</span>
         <span class="mx-3">Bootstrap</span>
@@ -7,9 +7,9 @@
         <span class="mx-3">PHP</span>
     </div>
     <div class="col d-flex align-items-center justify-content-center">
-        <p class="m-0">Cpyright &copy; 2023 <span class="text-danger">Rafael Cunha.</span> Todos os direitos reservados.</p>
+        <p class="m-0">Cpyright &copy; 2023 <span class="text-danger">Rafael Cunha.</span></span></p>
     </div>
-    <div class="col d-flex justify-content-end align-items-center d-lg-flex d-none">
+    <div class="col d-flex justify-content-end align-items-center d-xl-flex d-none d-lg-flex d-none">
         <p class="text-danger m-0">v 1.0</p>
     </div>
 </footer>
